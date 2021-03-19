f7672986c589edc7a963e4a158ebecbe5fcc5bcf
Ravindranadh varma <74560627+ravindranadh-varma@users.noreply.github.com>
2021-03-19 23:02:44 +0530

    Drum kit starting files

---
+++ index.js
@@ -0,0 +1,61 @@
+
+for(var i= 0;i<document.querySelectorAll(".drum").length;i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+        var buttonClicked = this.innerHTML;
+       makesound(buttonClicked);
+       buttonAnimated(buttonClicked);
+    })
+}
+document.addEventListener("keypress",function(event){
+   makesound(event.key);
+   buttonAnimated(event.key);
+})
+function makesound(key){
+    switch (key) {
+        case "w":
+            var ton1 = new Audio("sounds/tom-1.mp3");
+            ton1.play();            
+            break;
+
+        case "a":
+            var ton2 = new Audio("sounds/tom-2.mp3");
+            ton2.play();            
+            break;
+
+        case "s":
+           var ton3 = new Audio("sounds/tom-3.mp3");
+           ton3.play();            
+            break;
+
+        case "d":
+            var ton4= new Audio("sounds/tom-4.mp3");
+            ton4.play();            
+             break;
+
+        case "j":
+             var snare = new Audio("sounds/snare.mp3");
+            snare.play();            
+             break;
+
+        case "k":
+            var crash= new Audio("sounds/crash.mp3");
+            crash.play();            
+             break;
+
+        case "l":
+           var kick= new Audio("sounds/kick-bass.mp3");
+            kick.play();            
+            break;
+    default:
+        break;
+}
+}
+function buttonAnimated(currentkey){
+ var activeButton = document.querySelector("."+currentkey);
+
+ activeButton.classList.add("pressed");
+ setTimeout(function(){
+     activeButton.classList.remove("pressed");
+ },100);
+ 
+}
